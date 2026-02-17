ae4a012940745020d3b3b75701d24357c7ee8130
Flucke2543 <Poonawitmuenwat@gmail.com>
2026-02-17 14:54:31 +0700

    Update books.js

--- books.js
+++ books.js
@@ -1,1 +1,22 @@
-
+// books.js - คลังคัมภีร์ของ NA-AN
+const library = {
+    "001": {
+        title: "เสียงเรียกจากคธูลู",
+        author: "H.P. Lovecraft",
+        pdf: "library/cthulhu.pdf", // ชื่อไฟล์ PDF ที่มึงจะเอามาลง
+        cover: "assets/cover1.jpg"   // ชื่อไฟล์ปก
+    },
+    "002": {
+        title: "นครไร้นาม",
+        author: "H.P. Lovecraft",
+        pdf: "library/innsmouth.pdf",
+        cover: "assets/cover2.jpg"
+    }
+    "003": {
+        title: "เดกอน",
+        author: "H.P. Lovecraft",
+        pdf: "library/innsmouth.pdf",
+        cover: "assets/cover3.jpg"
+    }
+    // อนาคตจะเพิ่มอีกกี่ล้านเล่ม ก็แค่ก๊อปบรรทัด "002" มาวางต่อแล้วเปลี่ยนเลข
+};
