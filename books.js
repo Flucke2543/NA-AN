c593c03976e72f9c4aca9762dc0783269f5aeb31
Flucke2543 <Poonawitmuenwat@gmail.com>
2026-02-17 16:23:53 +0700

    Update books.js

--- books.js
+++ books.js
@@ -3,19 +3,19 @@ const library = {
     "001": {
         title: "เสียงเรียกจากคธูลู",
         author: "H.P. Lovecraft",
-        pdf: "library/cthulhu.pdf", // ชื่อไฟล์ PDF ที่มึงจะเอามาลง
+        pdf: "file:///E:/download/69490a7377f5b-the-little-duke-or-richard-the-fearless-by-charlotte-mary-yonge.pdf", // ชื่อไฟล์ PDF ที่มึงจะเอามาลง
         cover: "assets/cover1.jpg"   // ชื่อไฟล์ปก
     },
     "002": {
         title: "นครไร้นาม",
         author: "H.P. Lovecraft",
-        pdf: "library/innsmouth.pdf",
+        pdf: "file:///E:/download/69490a7377f5b-the-little-duke-or-richard-the-fearless-by-charlotte-mary-yonge.pdf",
         cover: "assets/cover2.jpg"
     }
     "003": {
         title: "เดกอน",
         author: "H.P. Lovecraft",
-        pdf: "library/innsmouth.pdf",
+        pdf: "file:///E:/download/69490a7377f5b-the-little-duke-or-richard-the-fearless-by-charlotte-mary-yonge.pdf",
         cover: "assets/cover3.jpg"
     }
     // อนาคตจะเพิ่มอีกกี่ล้านเล่ม ก็แค่ก๊อปบรรทัด "002" มาวางต่อแล้วเปลี่ยนเลข
