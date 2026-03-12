01ed35cc659dc43bcf3571580c139354eea61eee
Flucke2543 <Poonawitmuenwat@gmail.com>
2026-03-12 18:05:06 +0700

    Update books.js

--- books.js
+++ books.js
@@ -3,19 +3,19 @@ const library = {
     "001": {
         title: "เสียงเรียกจากคธูลู",
         author: "H.P. Lovecraft",
-        pdf: "library/The call of Cthulhu.pdf", // ชื่อไฟล์ PDF ที่มึงจะเอามาลง
-        cover: "https://raw.githubusercontent.com/Flucke2543/NA-AN/refs/heads/main/library/Cover/Call%20of%20Cthullu_cover.jpg"   // ชื่อไฟล์ปก
+        pdf: "‎library/data/The call of Cthulhu.pdf", 
+        cover: "https://raw.githubusercontent.com/Flucke2543/NA-AN/refs/heads/main/library/Cover/Call%20of%20Cthullu_cover.jpg"   /
     },
     "002": {
         title: "นครไร้นาม",
         author: "H.P. Lovecraft",
-        pdf: "library/The Nameless City.pdf",
+        pdf: "library/data/The Nameless City.pdf",
         cover: "https://raw.githubusercontent.com/Flucke2543/NA-AN/refs/heads/main/library/Cover/Dagon_cover.jpg"
     },
     "003": {
         title: "เดกอน",
         author: "H.P. Lovecraft",
-        pdf: "library/Dagon.pdf",
+        pdf: "library/data/Dagon.pdf",
         cover: "https://raw.githubusercontent.com/Flucke2543/NA-AN/refs/heads/main/library/Cover/The%20Nameless%20city_cover.jpg"
     } 
 };
