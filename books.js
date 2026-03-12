67d3be93f53051e03114ed170fdd6a3f523615c0
Flucke2543 <Poonawitmuenwat@gmail.com>
2026-03-12 18:39:05 +0700

    Update books.js

--- books.js
+++ books.js
@@ -3,7 +3,7 @@ const library = {
     "001": {
         title: "เสียงเรียกจากคธูลู",
         author: "H.P. Lovecraft",
-        pdf: "‎‎library/data/The call of Cthulhu.pdf", 
+        pdf: "‎‎library/data/The_call_of_Cthulhu.pdf", 
         cover: "https://raw.githubusercontent.com/Flucke2543/NA-AN/refs/heads/main/library/Cover/Call%20of%20Cthullu_cover.jpg" 
     },
     "002": {
